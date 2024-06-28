d4271d5b853e86cf78eae94a130588df3c8093e3
Viclor27 <vito27322@gmail.com>
2024-06-28 14:35:20 -0600

    modulos

--- src/config.js
+++ src/config.js
@@ -1,22 +1,22 @@
 import { config } from "dotenv";
 config();
 
-export default {
+/* export default {
   port: process.env.PORT || 3001,
-  dbUser: process.env.DB_USER || "User",
-  dbPassword: process.env.DB_PASSWORD || "User",
+  dbUser: process.env.DB_USER || "Userrr",
+  dbPassword: process.env.DB_PASSWORD || "Userrr",
   dbServer: process.env.DB_SERVER || "localhost",
   dbDatabase: process.env.DB_DATABASE || "BDPrototipo",
 };
-
-/* export default {
+ */
+export default {
  port: process.env.PORT || 3001,
   dbUser: process.env.DB_USER || "sqlserver",
   dbPassword: process.env.DB_PASSWORD || "sqlserver",
-  dbServer: process.env.DB_SERVER || "35.193.161.9",
+  dbServer: process.env.DB_SERVER || "35.192.19.210",
   dbDatabase: process.env.DB_DATABASE || "BDPrototipo",
   options: {
         encrypt: false,
         trustServerCertificate: false 
     }
-};  */
+};
