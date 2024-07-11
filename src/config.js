6b8946e03c8d4e46c3d9e3f473fa83b004c0256c
Viclor27322 <vito27322@gmail.com>
2024-07-11 16:23:06 -0600

    conexion

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 import { config } from "dotenv";
 config();
 
-export default {
+/* export default {
   port: process.env.PORT || 3001,
   dbUser: process.env.DB_USER || "Userrr",
   dbPassword: process.env.DB_PASSWORD || "Userrr",
   dbServer: process.env.DB_SERVER || "localhost",
   dbDatabase: process.env.DB_DATABASE || "BDPrototipo",
-};
+}; */
 
-/*
+
 export default {
  port: process.env.PORT || 3001,
   dbUser: process.env.DB_USER || "sqlserver",
@@ -20,4 +20,4 @@ export default {
         encrypt: false,
         trustServerCertificate: false 
     }
-};*/
+};
