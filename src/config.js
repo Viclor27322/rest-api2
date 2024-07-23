e497373b3a2a6dc0ef59d71073f73ccfb5c2d713
Viclor27322 <vito27322@gmail.com>
2024-07-23 09:33:57 -0600

    confirmacion

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
   dbPassword: process.env.DB_PASSWORD || "User",
   dbServer: process.env.DB_SERVER || "localhost",
   dbDatabase: process.env.DB_DATABASE || "BDPrototipo",
-};
+}
 
 
 /* export default {
