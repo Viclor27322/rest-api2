ef99be20e95f7175a895b11446cd55c009a1d925
Viclor27 <vito27322@gmail.com>
2024-07-24 14:39:31 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -21,11 +21,25 @@ app.set("port", config.port);
 
 
 // Middlewares
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://cirupied-eight.vercel.app'
+];
+
 app.use(cors({
-  origin: 'https://cirupied-eight.vercel.app',
+  origin: function (origin, callback) {
+    // allow requests with no origin, like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
