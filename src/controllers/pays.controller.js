a30e37b39c689d35475e918727d795d5319b0ebb
Viclor27 <vito27322@gmail.com>
2024-11-11 15:17:44 -0600

    Verificacion de ruta

---
+++ src/controllers/pays.controller.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const Stripe = require('stripe');
+const stripe = new Stripe(process.env.Stripe_Key); // Usa la variable de entorno
+
+// Importa la conexión a la base de datos
+import { getConnection } from "../database";
+
+// Guarda el pago exitoso en la base de datos
+async function guardarPagoEnHistorial(paymentIntent) {
+  const connection = await getConnection();
+  await connection('historial_pagos').insert({
+    pacienteId: paymentIntent.metadata.pacienteId,
+    amount: paymentIntent.amount,
+    status: paymentIntent.status,
+    paymentDate: new Date(),
+    paymentId: paymentIntent.id
+  });
+}
+
+export const Payment = async (req, res) => {
+  const { amount, pacienteId } = req.body; // Información que recibes del frontend
+
+  try {
+    // Crea el PaymentIntent con el monto y la moneda
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount, // Monto en centavos (por ejemplo, 10000 para 100 MXN)
+      currency: 'mxn', // Define la moneda
+      metadata: { pacienteId }, // Guarda información extra si es necesario
+    });
+
+    // Guarda el PaymentIntent en la base de datos
+    await guardarPagoEnHistorial(paymentIntent);
+
+    // Envía el client_secret al frontend para completar el pago
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
