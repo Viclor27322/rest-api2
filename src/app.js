a83875b2303c200c06aa461f1abdba76996d8035
Viclor27 <vito27322@gmail.com>
2024-11-11 16:10:48 -0600

    Cors

--- src/app.js
+++ src/app.js
@@ -29,15 +29,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin, like mobile apps or curl requests
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: '*', // Permitir todos los orígenes temporalmente
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
