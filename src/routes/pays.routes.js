0adc13a687f49c094d560065d7b7ccda84b4f0fc
Viclor27322 <126909284+Viclor27322@users.noreply.github.com>
2024-11-28 20:51:59 -0600

    Update pays.routes.js

--- src/routes/pays.routes.js
+++ src/routes/pays.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { Payment, createOrder, obtenerHistorialPagos} from "../controllers/pays.controller.js"
+import { Payment, PaymentAlevosia, createOrder, obtenerHistorialPagos} from "../controllers/pays.controller.js"
 
 const router =  Router();
 
 router.post('/create-payment-intent', Payment);
+router.post('/create-payment-alevosia', PaymentAlevosia);
 router.post('/payments/create-order', createOrder);
 router.get('/historial-pagos', obtenerHistorialPagos);
 export default router;
